99de15a9a988ebc2e02ebffe0dce00607fd2c51b
mohankadali123 <117376999+mohankadali123@users.noreply.github.com>
2024-01-08 12:06:58 +0530

    Update cat.py

--- cat.py
+++ cat.py
@@ -4,10 +4,10 @@ st.header("Vrindhavan")
 
 col1, col2 = st.columns(2)
 with col1:
-  st.subheader("Cat1")
+  st.subheader("krishna")
   st.image("./me1.jpeg", caption="Krishna ", width=300,use_column_width=True)
   st.write("krishna")
 with col2:
-  st.subheader("Cat2")
+  st.subheader("krishna")
   st.image("./me2.jpeg", caption="Krishna", width=300,use_column_width=True)
   st.write("krishna")
