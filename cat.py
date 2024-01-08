26c0c2ea0580ee06e4bbbf6e8e9ebd003f027e67
mohankadali123 <117376999+mohankadali123@users.noreply.github.com>
2024-01-08 12:05:17 +0530

    Update cat.py

--- cat.py
+++ cat.py
@@ -6,8 +6,8 @@ col1, col2 = st.columns(2)
 with col1:
   st.subheader("Cat1")
   st.image("./me1.jpeg", caption="Persian Cat", width=300,use_column_width=True)
-  st.write("Persian cats are cute")
+  st.write("krishna")
 with col2:
   st.subheader("Cat2")
   st.image("./me2.jpeg", caption="Ragdoll Cat", width=300,use_column_width=True)
-  st.write("Ragdoll cats are proud")
+  st.write("krishna")
